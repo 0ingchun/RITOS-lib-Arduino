94070de65db75b165f3560ea21a0333a3ebcd447
泠纯 <66431027+0ingchun@users.noreply.github.com>
2023-07-03 02:31:47 +0800

    Update RITOSbasic.ino

--- examples/RITOSbasic/RITOSbasic.ino
+++ examples/RITOSbasic/RITOSbasic.ino
@@ -1,4 +1,5 @@
-#include "ritos.h" //RITOS: Real Internet Time Operation Sansystem RITOS:实时互联网操作系统 for ESP8266
+#include "ritos.h" 
+// RITOS: Real Internet Time Operation Sansystem / RITOS:实时互联网操作系统 for ESP8266
 
 /*
   Please select esp8266 as the development board before compiling
@@ -6,52 +7,55 @@
 */
 
 volatile int a = 1000, b = 3000;
-int statemachine=0;
+int statemachine = 0;
 
-Ritos tarefa1;
+Ritos tarefa1; // Thread Task One / 綫程任務1
 void thread1() {
-  a--; 
+  a--;
   
   if ( a <= 0 && statemachine==0) {
    Serial.println("Interrupt thread 1!");
-   
-   a=1500; //1500ms
+    
+   // Attention! Delay() cannot be used within the thread / 注意！綫程内不允許使用Delay()
+   a = 1500; // 1500ms. You can be used in this way instead of Delay(); 您可以通過此方法延時，代替Delay();
    statemachine=1;
   }
 
   if ( a <= 0 && statemachine==1) {   
    Serial.println("thread 1 again!");
    
-   a=2000; 
+   a = 2000; // 2000ms
    statemachine=2;
   }
 
    if ( a <= 0 && statemachine==2) {   
    Serial.println("thread 1 again again!");
    
-   a=1700; 
+   a = 1700; // 1700ms
    statemachine=0;
   }
 }
 
 
-Ritos tarefa2;
+Ritos tarefa2; // Thread Task Two / 綫程任務2
 void thread2() {
   b--; 
   if ( b <= 0 ) {
    
   Serial.println("Interrupcao thread 2!");
-  b=4300; 
+  b = 4300; // 4300ms
   }
 }
 
 void setup() {
   Serial.begin(115200);
 
-  // Criar threads concorrentes Criar线程一致
+  // Create threads task / 創建线程任務
   tarefa1.task(thread1);  
   tarefa2.task(thread2);
 
 }
 
-void loop() {}
+void loop() {
+  // Nothing will be run here / 此處不會被運行任何東西
+}
